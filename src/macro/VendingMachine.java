ff7a10eb15e4f2e43d078fa3f77ff7f091b0cde6
Dmitriy Semenov <dvsemen93@gmail.com>
2024-03-20 23:23:35 +0500

    Update VendingMachine.java

--- src/macro/VendingMachine.java
+++ src/macro/VendingMachine.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public interface VendingMachine {
 
-
+//я хотел создать здесь поле с коллекцией продуктов, который инициализируется через конструктор.
+//но не понял можно ли так сделать(у меня не получилось). Можно ли реализовать таккой функционал 
     HotDrink getProduct(String name, int volume, int temperature);
 
 }
